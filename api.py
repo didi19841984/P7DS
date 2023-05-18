5360c34376bc51baaa72907089973af79beafb6b
didi19841984 <133697985+didi19841984@users.noreply.github.com>
2023-05-19 00:49:47 +0200

    Update api.py

--- api.py
+++ api.py
@@ -6,4 +6,7 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=81)
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
